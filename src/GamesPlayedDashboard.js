01397438b7c5e3773290290ad58808044d4f230a
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-22 17:20:36 -0400

    Implements static PlayerSubmissionForm

--- src/GamesPlayedDashboard.js
+++ src/GamesPlayedDashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SectionHeader from './SectionHeader'
+import PlayerSubmissionForm from './PlayerSubmissionForm'
 
 class GamesPlayedDashboard extends React.Component {
 	render() {
 		return (
 			<div>
 				<SectionHeader headerText={'Add a Player'} />
+				<PlayerSubmissionForm />
 				{
 				//<SectionHeader />
 				//<PlayerSubmissionForm />
