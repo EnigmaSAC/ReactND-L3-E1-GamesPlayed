b9a9902a0f86c8e2ab6ab4b9e7d476ff54db4fbf
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-22 17:00:07 -0400

    Implements SectionHeader in dashboard

--- src/GamesPlayedDashboard.js
+++ src/GamesPlayedDashboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import SectionHeader from './SectionHeader'
 
 class GamesPlayedDashboard extends React.Component {
 	render() {
 		return (
 			<div>
+				<SectionHeader headerText={'Add a Player'} />
 				{
 				//<SectionHeader />
 				//<PlayerSubmissionForm />
