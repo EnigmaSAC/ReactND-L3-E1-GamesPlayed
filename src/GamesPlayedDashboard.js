d4e6ec9ae62b890eff51d1418107010c00cace45
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-22 16:45:39 -0400

    Adds GamesPlayedDashboard's basic implementation

---
+++ src/GamesPlayedDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class GamesPlayedDashboard extends React.Component {
+	render() {
+		return (
+			//<SectionHeader />
+			//<PlayerSubmissionForm />
+			//<PlayersList />
+			//<ShowOrHideButton />
+		)
+	}
+}
+
+export default GamesPlayedDashboard
