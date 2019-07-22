ec84512db3471ba16f00eb5770a66866ff9b176f
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-22 16:50:01 -0400

    imports and calls GamesPlayedDashboard on App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GamesPlayedDashboard from './GamesPlayedDashboard'
 
 /*
 This exercise will help you put together and practice all of the concepts you've
@@ -18,7 +19,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-
+        <GamesPlayedDashboard />
       </div>
     );
   }
