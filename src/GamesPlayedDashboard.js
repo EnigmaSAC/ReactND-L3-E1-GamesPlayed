d27b4f809ad6ea25596475b9b9e37bd44b6007f6
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-24 12:37:00 -0400

    Implements ShowOrHideButton on Dashboard

--- src/GamesPlayedDashboard.js
+++ src/GamesPlayedDashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SectionHeader from './SectionHeader'
 import PlayerSubmissionForm from './PlayerSubmissionForm'
 import PlayersList from './PlayersList'
+import ShowOrHideButton from './ShowOrHideButton'
 
 class GamesPlayedDashboard extends React.Component {
 	render() {
@@ -10,6 +11,7 @@ class GamesPlayedDashboard extends React.Component {
 				<SectionHeader headerText={'Add a Player'} />
 				<PlayerSubmissionForm />
 				<PlayersList />
+				<ShowOrHideButton onClickHandler={()=> console.log('Show Or Hide clicked!')} buttonText={'Hide the Number of Games Played'} />
 				{
 				//<SectionHeader />
 				//<PlayerSubmissionForm />
