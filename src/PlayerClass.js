3d2761ef323f5c31da589eeaa00ff40be028ff00
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-24 13:38:40 -0400

    Adds PlayerClass.js to project

---
+++ src/PlayerClass.js
@@ -0,0 +1,10 @@
+class Player {
+	constructor (firstName, lastName, username) {
+		this.firstName = firstName
+		this.lastName = lastName
+		this.username = username
+		this.gamesCount = 0
+	}
+}
+
+export default Player
