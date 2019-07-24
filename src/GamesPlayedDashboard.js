dc24d084b4f3d7bbabde2036680e18f5a8e63657
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-24 13:42:13 -0400

    Adds State to Dashboard and uses it

--- src/GamesPlayedDashboard.js
+++ src/GamesPlayedDashboard.js
@@ -3,14 +3,23 @@ import SectionHeader from './SectionHeader'
 import PlayerSubmissionForm from './PlayerSubmissionForm'
 import PlayersList from './PlayersList'
 import ShowOrHideButton from './ShowOrHideButton'
+import Player from './PlayerClass'
 
 class GamesPlayedDashboard extends React.Component {
+	State = {
+		players:[
+			{firstName: 'Sergio', lastName: 'Cabral', username: 'scabralo', gamesCount: 0},
+			{firstName: 'Antonio', lastName: 'Ogando', username: 'enigmasac', gamesCount: 0},
+		],
+		showGames: true
+	}
 	render() {
+		const {players, showGames} = this.State
 		return (
 			<div>
 				<SectionHeader headerText={'Add a Player'} />
-				<PlayerSubmissionForm />
-				<PlayersList players={[],[],[]} />
+				<PlayerSubmissionForm  />
+				<PlayersList players={players} showGames={showGames} />
 				<ShowOrHideButton onClickHandler={()=> console.log('Show Or Hide clicked!')} buttonText={'Hide the Number of Games Played'} />
 			</div>
 		)
